b70623e8d8cc1ceb6d81115adade0891f887ef2b
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-27 00:19:52 +0530

    created header component

---
+++ components/Header.jsx
@@ -0,0 +1,53 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+    const headerMenu = [
+        {
+            id: 1,
+            name: 'Ride',
+            icon: '/taxi.jpeg'
+        },
+        {
+            id: 2,
+            name: 'Package',
+            icon: '/box.jpeg'
+        }
+    ]
+    return (
+        <div className='p-5 pb-3 pl-10 border-b-[2px] border-gray-300 flex items-center justify-between'>
+            <div className='flex gap-24 items-center'>
+                <div className="flex items-center">
+                    <Image
+                        src='/logo.png'
+                        width={80} height={80}
+                        alt='Logo' className='cursor-pointer'
+                    />
+                    <span className="italic text-2xl font-bold ml-3">
+                        Speedy.
+                        <span className="text-green-500 text-2xl font-bold py-1">Go</span>
+                    </span>
+                </div>
+
+                <div className='flex gap-8 items-center'>
+                    {
+                        headerMenu.map((item) => (
+                            <div className='flex gap-2 items-center' key={item.name}>
+                                <Image
+                                    src={item.icon}
+                                    width={25} height={25}
+                                />
+                                <h2 className='text-[14px] font-medium'>{item.name}</h2>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <UserButton />
+        </div>
+
+    )
+}
+
+export default Header
