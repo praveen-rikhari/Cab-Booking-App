0cc2d1589563124f0093fd59eac26dd1250eaeba
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-28 00:30:31 +0530

    adding vechiles in UI

--- components/Booking/Booking.jsx
+++ components/Booking/Booking.jsx
@@ -1,18 +1,19 @@
 'use client'
 import React from 'react';
 import AutoCompleteAddress from './AutoCompleteAddress';
+import Cars from './Cars';
 
 function Booking() {
     // const screenHeight = window.innerHeight * 0.72; style={{ height: screenHeight }}
     return (
         <div>
             <div className="p-2 md:p-6 border-[2px] rounded-xl" >
-                <p className="text-[22px] font-bold">
+                <p className="text-[24px] font-bold">
                     Book a Ride
                 </p>
 
                 <AutoCompleteAddress />
-
+                <Cars/>
                 <button className=' p-4 bg-black w-full mt-5 text-white font-bold rounded-lg'>
                     Search Rides
                 </button>
---
+++ components/Booking/Cars.jsx
@@ -0,0 +1,43 @@
+import CarsList from '@/data/CarsList'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+function Cars() {
+    const [selectedCar, setSelectedCar] = useState();
+    return (
+        <div className='mt-3'>
+            <h2 className='font-semibold text-[20px] '>Select a Vechile</h2>
+            <div className='grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    CarsList.map((item, index) => (
+                        <div key={index}
+                            className={
+                                `m-2 p-2 border-[2px] rounded-md hover:border-yellow-400 cursor-pointer 
+                                    ${index == selectedCar
+                                    ? 'border-yellow-400 border-[3px]'
+                                    : null
+                                }`
+                            }
+                            onClick={() => setSelectedCar(index)}
+                        >
+                            <Image src={item.image}
+                                alt={item.name}
+                                width={75}
+                                height={90}
+                                className='w-full'
+                            />
+                            <h2 className='text-[15px] text-gray-500'>{item.name}
+
+                                <span className='float-right font-medium text-black'>
+                                    ${item.charges * 8}
+                                </span>
+                            </h2>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Cars
