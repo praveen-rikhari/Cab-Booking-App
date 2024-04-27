1a9f57dbffcd2cc6e56512ed57a266eb03185f36
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-27 21:43:24 +0530

    autocompletion of source's address

--- components/Booking/AutoCompleteAddress.jsx
+++ components/Booking/AutoCompleteAddress.jsx
@@ -1,9 +1,38 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function AutoCompleteAddress() {
+
+    const [source, setSource] = useState()
+    const [addressList, setAddressList] = useState([]);
+
+    const [sourceChange, setSourceChange] = useState(false)
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            getAddressList()
+        }, 1000)
+
+        return () => clearTimeout(delayDebounceFn)
+
+    }, [source]);
+
+
+    const getAddressList = async () => {
+        setAddressList([]);
+
+        const res = await fetch('/api/search-address?q=' + source, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        const result = await res.json();
+        setAddressList(result)
+
+    }
     return (
-        <>
+        <div className='relative'>
             <div className='bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4'>
                 <Image src={'/source.png'} width={15} height={15} />
 
@@ -11,8 +40,35 @@ function AutoCompleteAddress() {
                     type="text"
                     placeholder={'Pickup Location'}
                     className='bg-transparent w-full outline-none'
+                    value={source}
+                    onChange={(e) => {
+                        setSource(e.target.value)
+                        setSourceChange(true)
+                    }}
                 />
             </div>
+
+            {
+                addressList?.suggestions && sourceChange ?
+                    <div className='shadow-md p-1 rounded-md absolute w-full bg-white z-20'>
+                        {
+                            addressList?.suggestions.map((item, index) => (
+                                <h2
+                                    key={index}
+                                    className='p-3 hover:bg-gray-100 cursor-pointer'
+                                    onClick={() => {
+                                        setSource(item.full_address);
+                                        setAddressList([]);
+                                        setSourceChange(false)
+                                    }}
+                                >
+                                    {item.full_address}
+                                </h2>
+                            ))
+                        }
+                    </div> : null
+            }
+
             <div className='bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4'>
                 <Image src={'/dest.png'} width={15} height={15} />
 
@@ -22,7 +78,7 @@ function AutoCompleteAddress() {
                     className='bg-transparent w-full outline-none'
                 />
             </div>
-        </>
+        </div>
     )
 }
 
