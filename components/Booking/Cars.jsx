22c3774c9ec62b69bed6b8043c456fd53345ae0b
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-30 20:43:44 +0530

    Calculating fare charges

--- components/Booking/Cars.jsx
+++ components/Booking/Cars.jsx
@@ -1,13 +1,20 @@
+import { DirectionDataContext } from '@/context/DirectionDataContext';
 import CarsList from '@/data/CarsList'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 
 function Cars() {
     const [selectedCar, setSelectedCar] = useState();
+    const { directionData, setDirectionData } = useContext(DirectionDataContext);
+
+    const getCost = (charges) => {
+        return (charges * directionData.routes[0].distance * 0.001).toFixed(2)
+    }
+
     return (
         <div className='mt-3'>
             <h2 className='font-semibold text-[18px]'>Select a Vechile</h2>
-            
+
             <div className='grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     CarsList.map((item, index) => (
@@ -29,9 +36,13 @@ function Cars() {
                             />
                             <h2 className='text-[15px] text-gray-500'>{item.name}
 
-                                <span className='float-right font-medium text-black'>
-                                    ${item.charges * 8}
-                                </span>
+                                {
+                                    directionData.routes ?
+                                        <span className='float-right font-medium text-black'>
+                                            $ {getCost(item.charges)}
+                                        </span> : null
+                                }
+
                             </h2>
                         </div>
                     ))
