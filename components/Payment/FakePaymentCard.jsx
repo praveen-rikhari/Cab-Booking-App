dfd8bea6c5fa744448d8c4f7e826c7507f8beb72
praveen-rikhari <praveenrikhari45@gmail.com>
2024-05-10 23:48:03 +0530

    minor ui changes in fake payment card

--- components/Payment/FakePaymentCard.jsx
+++ components/Payment/FakePaymentCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FakePaymentCard = () => {
     return (
-        <div className="max-w-md mx-auto bg-yellow-100 border border-yellow-300 rounded-lg m-6 p-4">
+        <div className="max-w-md mx-auto bg-red-100 border border-red-300 rounded-lg m-6 p-4">
             <div className="text-sm font-semibold mb-2 text-red-500">NOTE: Use the following fake card details for payment</div>
             <div className="flex space-x-4">
                 <div className="w-1/2">
@@ -11,7 +11,7 @@ const FakePaymentCard = () => {
                         type="text"
                         value="4242 4242 4242 4242"
                         readOnly
-                        className="w-full bg-yellow-200 border border-yellow-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
+                        className="w-full bg-red-200 border border-red-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
                     />
                 </div>
                 <div className="w-1/5">
@@ -20,7 +20,7 @@ const FakePaymentCard = () => {
                         type="text"
                         value="12/26"
                         readOnly
-                        className="w-full bg-yellow-200 border border-yellow-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
+                        className="w-full bg-red-200 border border-red-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
                     />
                 </div>
                 <div className="w-1/6">
@@ -29,7 +29,7 @@ const FakePaymentCard = () => {
                         type="text"
                         value="123"
                         readOnly
-                        className="w-full bg-yellow-200 border border-yellow-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
+                        className="w-full bg-red-200 border border-red-300 rounded py-1 px-2 focus:outline-none focus:bg-white"
                     />
                 </div>
             </div>
