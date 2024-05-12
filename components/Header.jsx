7e42ab139a40e70f765790ac55f5d39626813fc3
praveen-rikhari <praveenrikhari45@gmail.com>
2024-05-12 23:29:37 +0530

    Updating navbar component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,47 +1,29 @@
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
-    const headerMenu = [
-        {
-            id: 1,
-            name: 'Ride',
-            icon: '/taxi.jpeg'
-        },
-        {
-            id: 2,
-            name: 'Package',
-            icon: '/box.jpeg'
-        }
-    ]
     return (
         <div className='p-5 pb-3 pl-10 border-b-[2px] border-gray-300 flex items-center justify-between'>
             <div className='flex gap-24 items-center'>
+
                 <div className="flex items-center">
-                    <Image
-                        src='/logo.png'
-                        width={80} height={80}
-                        alt='Logo' className='cursor-pointer'
-                    />
+                    <Link legacyBehavior href="/">
+                        <a className="cursor-pointer" style={{ textDecoration: 'none' }}>
+                            <Image
+                                src='/logo.png'
+                                width={80}
+                                height={80}
+                                alt='Logo'
+                                className='cursor-pointer'
+                            />
+                        </a>
+                    </Link>
                     <span className="text-2xl font-bold ml-3 italic">
                         Speedy.
-                        <span className="text-yellow-400 text-2xl font-bold py-1" style={{fontStyle : 'normal'}}>Go</span>
+                        <span className="text-yellow-400 text-2xl font-bold py-1" style={{ fontStyle: 'normal' }}>Go</span>
                     </span>
-                </div>
-
-                <div className='flex gap-8 items-center'>
-                    {
-                        headerMenu.map((item) => (
-                            <div className='flex gap-2 items-center' key={item.name}>
-                                <Image
-                                    src={item.icon}
-                                    width={25} height={25}
-                                />
-                                <h2 className='text-[14px] font-medium'>{item.name}</h2>
-                            </div>
-                        ))
-                    }
                 </div>
             </div>
             <UserButton />
@@ -50,4 +32,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
