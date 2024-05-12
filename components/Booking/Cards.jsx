72447d9d633f7e55579225b390d00651113ab2f4
praveen-rikhari <praveenrikhari45@gmail.com>
2024-05-12 23:45:52 +0530

    Updating home page layout

--- components/Booking/Cards.jsx
+++ components/Booking/Cards.jsx
@@ -1,39 +1,39 @@
-import CardsList from '@/data/CardsList'
-import Image from 'next/image'
-import React, { useState } from 'react'
+import CardsList from '@/data/CardsList';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 function Cards() {
     const [activeIndex, setActiveIndex] = useState();
+
     return (
-        <div>
-            <h2 className='font-semibold text-[18px]'>
+        <div className="px-4 md:px-0">
+            <h2 className="font-semibold text-2xl md:text-3xl">
                 Payment Methods
             </h2>
 
-            <div className='grid grid-cols-5 md:grid-cols-4 lg:grid-cols-5 mt-2 pl-2'>
-                {
-                    CardsList.map((item, index) => (
-                        <div
-                            key={index}
-                            className={
-                                `w-[75px] mb-1 border-[2px] flex items-center justify-center rounded-md cursor-pointer hover:border-yellow-400 hover:scale-110 transition-all
-                                ${activeIndex == index
-                                    ? 'border-yellow-400 border-[2px]' : null}`
-                            }
-                            onClick={() => setActiveIndex(index)}>
+            <div className="flex flex-wrap gap-2 mt-4">
+                {CardsList.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`w-20 h-12 md:w-20 md:h-10 border-[2px]
+                         flex items-center justify-center rounded-md
+                          cursor-pointer hover:border-yellow-400 
+                          hover:scale-110 transition-all
+                           ${activeIndex === index ? 'border-yellow-400' : 'border-gray-300'}`}
 
-                            <Image
-                                src={item.image}
-                                alt={item.name}
-                                width={40}
-                                height={50}
-                            />
-                        </div>
-                    ))
-                }
+                        onClick={() => setActiveIndex(index)}
+                    >
+                        <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={40}
+                            height={50}
+                        />
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Cards;
